b89b2e8da0fbecf00715bf9fc2acb1ca6e996256
Tarek Rahman <36471840+Tarekrahman5500@users.noreply.github.com>
2025-04-08 22:46:50 +0600

    mutation

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,11 +1,31 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
-import { Coffee } from '../graphql';
+import { Coffee, CoffeeCreateInput } from '../graphql';
 
 @Resolver()
 export class CoffeesResolver {
   @Query('coffees')
   coffeeFindAll(): Coffee[] {
     return [];
+  }
+
+  @Query('coffee')
+  coffeeFindOne(@Args('id') id: number): Coffee {
+    return {
+      id: id,
+      name: 'test',
+      brand: 'test',
+      flavors: ['test'],
+    };
+  }
+
+  @Mutation('coffeeCreate')
+  coffeeCreate(
+    @Args('coffeeCreateInput') coffeeCreateInput: CoffeeCreateInput,
+  ): Coffee {
+    return {
+      id: 1,
+      ...coffeeCreateInput,
+    };
   }
 }
